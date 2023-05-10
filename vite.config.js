808aaae95320b1b72bd03b154c2ab22da364294d
Rivan Nurdin <rivannurdin001@gmail.com>
2023-05-10 16:52:40 +0700

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/rivan-codes.github.io/',
   plugins: [vue()],
   resolve: {
     alias: {
