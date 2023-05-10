fd0a52f568d68f24ce60820e630da29b32055074
Rivan Nurdin <34261972+rivan-codes@users.noreply.github.com>
2023-05-10 16:58:29 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/rivan-codes.github.io/',
+  base:'/',
   plugins: [vue()],
   resolve: {
     alias: {
